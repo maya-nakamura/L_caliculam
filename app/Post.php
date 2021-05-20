f2d095a986d4005fe8ecaa5227a9a4d44817408d
maya-nakamura <maya2021se@gmail.com>
2021-05-20 10:32:58 +0900

    210520

--- app/Post.php
+++ app/Post.php
@@ -1,31 +1,19 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App;
 
-use App\Post;
-use App\Http\Requests\PostRequest;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class PostController extends Controller
+class Post extends Model
 {
-    public function index(Post $post)
+    protected $fillable = [
+        'title',
+        'body'
+    ];
+    
+    public function getPaginate()
     {
-        return view('index')->with(['posts' => $post->getPaginate()]);
-    }
-
-    public function show(Post $post)
-    {
-        return view('show')->with(['post' => $post]);
-    }
-
-    public function create()
-    {
-        return view('create');
-    }
-
-    public function store(Post $post, PostRequest $request) // 引数をRequest->PostRequestにする
-    {
-        $input = $request['post'];
-        $post->fill($input)->save();
-        return redirect('/posts/' . $post->id);
+        return $this->orderBy('updated_at', 'DESC')->paginate(5);
     }
 }
